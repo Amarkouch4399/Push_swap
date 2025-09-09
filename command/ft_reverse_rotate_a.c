a62e812e32706bb0966e7ca01d9b81823179dca1
Amarkouch4399 <oussama.amarkouch4399@gmail.com>
2025-09-09 18:23:30 +0200

    add rra

--- command/ft_reverse_rotate_a.c
+++ command/ft_reverse_rotate_a.c
@@ -6,11 +6,28 @@
 /*   By: ouamarko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 18:23:06 by ouamarko          #+#    #+#             */
-/*   Updated: 2025/09/04 18:38:37 by ouamarko         ###   ########.fr       */
+/*   Updated: 2025/09/09 18:08:18 by ouamarko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_rotate_reverse_a(t_list **stack_a)
 {
+	t_list	*first;
+	t_list	*last;
+	t_list	*prev;
 
+	first = *stack_a;
+	last = *stack_a;
+	if (!stack_a || !*stack_a || !(*stack_a)->next)
+		return ;
+	while (last->next != NULL)
+	{
+		if (last->next == NULL)
+			prev = last;
+		last = last->next;
+	}
+	*stack_a = last;
+	first->next = last;
+	last->next = NULL;
+	printf("rra\n");
 }
